668c2e6b806c9e0021d265ccda0af2309bf5d2e2
TechTipsYt <91360006+TechTipsYt@users.noreply.github.com>
2022-02-02 21:52:09 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,5 +1,12 @@
 let firebaseConfig = {
     // Enter your firebase credentials
+     apiKey: "AIzaSyBtdH3DP85UDOA_Yrot9mYHlZGz3ctw2MY",
+    authDomain: "tech-website-def0e.firebaseapp.com",
+    projectId: "tech-website-def0e",
+    storageBucket: "tech-website-def0e.appspot.com",
+    messagingSenderId: "571097362080",
+    appId: "1:571097362080:web:9d2edf2f989823030ca3c5",
+    measurementId: "G-KF013MXF8L"
 };
 
 firebase.initializeApp(firebaseConfig);
